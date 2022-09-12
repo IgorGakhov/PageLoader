e1a1fa5a9d88c7edb3c07f40a558c56cfc6a2331
Igor Gakhov <gakhov_igor@rambler.ru>
2022-09-12 10:08:03 +0300

    Return exit status test for help command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,6 +6,11 @@ import pytest
 from page_loader import cli
 
 
+def test_help_command():
+    exit_status = os.system('page-loader -h')
+    assert exit_status == 0
+
+
 def test_cli_without_args():
     with pytest.raises(SystemExit) as error:
         cli.parse_arguments()()
